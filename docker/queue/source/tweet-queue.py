90bbd2c9c7d5e0974f418878f4cca7c69bc01332
fernandogbi <fernandosilva.kadima@gmail.com>
2020-04-04 10:47:05 -0300

    starting with chars of word count

--- docker/queue/source/tweet-queue.py
+++ docker/queue/source/tweet-queue.py
@@ -1,11 +1,13 @@
 from redis import Redis
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from flask import request
 import requests
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app, prefix = '/api/v1')
 
 class Cache(Resource):
