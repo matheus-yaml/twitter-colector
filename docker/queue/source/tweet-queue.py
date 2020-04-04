d53079ba6038806fe3a53317b8dd7cf30d4a4a8b
fernandogbi <fernandosilva.kadima@gmail.com>
2020-04-04 09:25:12 -0300

    creatind endpoint to return word counts

--- docker/queue/source/tweet-queue.py
+++ docker/queue/source/tweet-queue.py
@@ -9,11 +9,26 @@ app = Flask(__name__)
 api = Api(app, prefix = '/api/v1')
 
 class Cache(Resource):
+    def get(self):
+        conn = Redis(host='redis',port=6379,db=0)
+        results = {}
+        for i in conn.keys():
+            if int(conn.get(i)) > 1:
+                results[i.decode(encoding="utf-8")] = int(conn.get(i))
+        return results, 200
+
+
     def post(self):
+        conn = Redis(host='redis',port=6379,db=0)
         response = requests.post("http://counter:8080/api/v1/counter", data = {"tweet":request.form['text']})
-        app.logger.info(eval(response.content))
+        words = eval(response.content)['result']
+        for word in words:
+            if conn.get(word) == None:
+                conn.set(word,1)
+            else:
+                conn.set(word,int(conn.get(word))+1)
         return 200
 
-api.add_resource(Cache,'/cache',methods=['POST'])
+api.add_resource(Cache,'/cache',methods=['POST','GET'])
 
 app.run(host='0.0.0.0', port=5000, debug=True)
